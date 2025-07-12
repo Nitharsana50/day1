59b2d3a2d1f4a5be50d761405d5deb228b34515d
Nitharsana50 <24bct134@kgcas.com>
2025-07-12 10:47:38 +0530

    Add files via upload

---
+++ jss day1/index.js
@@ -0,0 +1,18 @@
+var num=12;
+var name='js';
+var iscloudy=true;
+console.log(num+" "+name+" "+iscloudy);
+document.write(num);
+document.write(name);
+document.write(iscloudy);
+//task2
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+//task3
+var num1=parseInt(prompt("Enter the first value"));
+var num2=parseInt(prompt("Enter the second value"));
+alert(num1+num2);
+console.log(num1*num2);
+console.log(num2-num1);
+console.log(num1/num2);
